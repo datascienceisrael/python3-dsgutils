b14566efe5f0db1d9bac6d60f5239df8cb8b518d
Elior Cohen <elior@datascience.co.il>
2017-12-06 10:40:12 +0200

    Initial commit, no readme

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='dsgutils',
+      version='0.1.0',
+      description='Utility functions for common data science operations and visualizations',
+      url='https://github.com/datascienceisrael/python3-dsgutils',
+      author='Data Science Group',
+      author_mail='elior@datascience.co.il',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pandas',
+      ],
+      zip_safe=False)
