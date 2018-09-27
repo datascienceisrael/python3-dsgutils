dbc7deacd93c22b884480c9292038e488001c10e
Elior Cohen <elior@datascience.co.il>
2018-09-27 13:31:38 +0300

    Updated version 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dsgutils',
-      version='0.1.7',
+      version='0.2.0',
       description='Utility functions for common data science operations and visualizations',
       url='https://github.com/datascienceisrael/python3-dsgutils',
       author='Data Science Group',
